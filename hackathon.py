c7a33f260fe4f8e1c5d78d97dac724971211bd2f
DEEPAN2000 <79573901+DEEPAN2000@users.noreply.github.com>
2021-05-14 20:11:20 +0530

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -1,12 +1,26 @@
-n= 9
-sock_list=[10,20,20,10]
+def find_pairs(arr,v):
+    unique=[]
+    for each in arr:                #also can be done using set() function to get unique elements
+        if(each not in unique):
+            unique.append(each)
+    
+    count_pair=[]
+    
+    for each in unique:             #finding number of occurances in the list
+        count_pair.append(arr.count(each)) 
+    print(count_pair) 
+    
+    final_count=0
+    
+    for each in count_pair:         #finding number of pairs
+        final_count+=each//2
+    return final_count        
+            
+    """for each in unique:               Alternative method
+        pair_count+=arr.count(each)//2
+      print(pair_count) 
+      """
 
-def sock_merchant(n,socks_list):
-    socks_list =set(sock_list)
-    count=0
-    for sock in sock_set:
-        count += sock_list.count{sock} // 2
-return count
-
-
-sock_merchants(n,socks_lists)
+n=10
+sock_list=[10,20,20,10,10,30,50,10,20,20]       
+print(find_pairs(sock_list, n))  
